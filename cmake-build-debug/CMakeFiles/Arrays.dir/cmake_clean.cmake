770e484b4e195c6075fdd63d9c6c7bc90e1f6449
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2020-09-18 22:14:51 +0300

    task array 101

---
+++ cmake-build-debug/CMakeFiles/Arrays.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "Arrays.exe"
+  "Arrays.exe.manifest"
+  "Arrays.pdb"
+  "CMakeFiles/Arrays.dir/main.cpp.obj"
+  "libArrays.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Arrays.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
