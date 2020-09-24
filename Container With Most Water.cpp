919b454defb5d462d099da8b9898d5835af40821
SyntaxErrorLineNULL <35173414+SyntaxErrorLineNULL@users.noreply.github.com>
2020-09-24 16:18:21 +0300

    Update Container With Most Water.cpp
    add solution Time Limit Exceeded task

--- Container With Most Water.cpp
+++ Container With Most Water.cpp
@@ -1,1 +1,23 @@
-
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int maxarea = 0, l = 0, r = height.size() - 1;
+        while (l < r) {
+            maxarea = max(maxarea, min(height[l], height[r]) * (r - l));
+            if (height[l] < height[r])
+                l++;
+            else
+                r--;
+        }
+        return maxarea;
+    }
+    // This code Time Limit Exceeded
+    int maxArea(vector<int>& height) {
+        int maxarea = INT32_MIN;
+        for(int i = 0; i < height.size(); i++){
+            for(int j = i + 1; j < height.size(); j++)
+                maxarea = max(maxarea, min(height[i], height[j]) * (j - i));
+        }
+        return maxarea;
+    }
+};
