e33a208917e6254870800f8ff9ec56abe5cb3d15
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2021-11-02 23:22:55 +0300

    Employees Earning More Than Their Managers 182 problem

--- DataBase/solution.sql
+++ DataBase/solution.sql
@@ -31,3 +31,17 @@ select a.Name as 'Employee'
 select a.Name as 'Employee'
     from Employee as a join Employee as b
         on a.ManagerId = b.Id and a.Salary > b.Salary
+
+/**
+    Duplicate Emails
+ */
+Create table If Not Exists Person (id int, email varchar(255))
+Truncate table Person
+insert into Person (id, email) values ('1', 'a@b.com')
+insert into Person (id, email) values ('2', 'c@d.com')
+insert into Person (id, email) values ('3', 'a@b.com')
+
+select Email
+    from Person
+        group by Email
+            having count(Email) > 1
