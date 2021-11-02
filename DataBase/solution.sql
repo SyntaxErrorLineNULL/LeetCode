b31eb0c974fcecf37fa8d0924334eea0a2e629b1
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2021-11-02 23:47:44 +0300

    Employees Earning More Than Their Managers 627 problem

--- DataBase/solution.sql
+++ DataBase/solution.sql
@@ -79,3 +79,17 @@ select name, population, area
     from World
         where area > 3000000 or population > 25000000
 
+/**
+  Swap Salary
+ */
+Create table If Not Exists Salary (id int, name varchar(100), sex char(1), salary int)
+Truncate table salary
+insert into salary (id, name, sex, salary) values ('1', 'A', 'm', '2500')
+insert into salary (id, name, sex, salary) values ('2', 'B', 'f', '1500')
+insert into salary (id, name, sex, salary) values ('3', 'C', 'm', '5500')
+insert into salary (id, name, sex, salary) values ('4', 'D', 'f', '500')
+
+update salary
+set sex = CASE sex
+      when 'm' then 'f' else 'm'
+end;
