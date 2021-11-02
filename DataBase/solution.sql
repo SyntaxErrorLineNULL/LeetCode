9f1b0afd45c1de4d08ea27b66bc8ef8acbb20328
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2021-11-02 23:16:33 +0300

    Employees Earning More Than Their Managers 181 problem

--- DataBase/solution.sql
+++ DataBase/solution.sql
@@ -1,4 +1,6 @@
-/** Combine Two Tables **/
+/**
+  Combine Two Tables
+  */
 Create table If Not Exists Person (personId int, firstName varchar(255), lastName varchar(255))
 Create table If Not Exists Address (addressId int, personId int, city varchar(255), state varchar(255))
 Truncate table Person
@@ -10,3 +12,22 @@ insert into Address (addressId, personId, city, state) values ('2', '3', 'Leetco
 
 SELECT firstname, lastname, city, state
     FROM Person LEFT JOIN Address ON Person.personid = Address.personid
+
+/**
+  Employees Earning More Than Their Managers
+ */
+Create table If Not Exists Employee (id int, name varchar(255), salary int, managerId int)
+Truncate table Employee
+insert into Employee (id, name, salary, managerId) values ('1', 'Joe', '70000', '3')
+insert into Employee (id, name, salary, managerId) values ('2', 'Henry', '80000', '4')
+insert into Employee (id, name, salary, managerId) values ('3', 'Sam', '60000', 'None')
+insert into Employee (id, name, salary, managerId) values ('4', 'Max', '90000', 'None')
+
+select a.Name as 'Employee'
+    from Employee as a, Employee as b
+         where a.ManagerId = b.Id and a.Salary > b.Salary
+
+/** Use join */
+select a.Name as 'Employee'
+    from Employee as a join Employee as b
+        on a.ManagerId = b.Id and a.Salary > b.Salary
