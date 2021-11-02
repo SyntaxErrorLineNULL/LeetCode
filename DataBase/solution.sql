d41cd0acac60caaa03357f6a85e3c19a3d9acf55
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2021-11-03 00:10:36 +0300

    Employees Earning More Than Their Managers 184 problem

--- DataBase/solution.sql
+++ DataBase/solution.sql
@@ -142,3 +142,32 @@ END
 
 /* Write your PL/SQL query statement below */
 SELECT score, DENSE_RANK() OVER (ORDER BY score DESC) AS Rank FROM scores ORDER BY score DESC;
+
+/**
+    Department Highest Salary
+ */
+Create table If Not Exists Employee (id int, name varchar(255), salary int, departmentId int)
+Create table If Not Exists Department (id int, name varchar(255))
+Truncate table Employee
+insert into Employee (id, name, salary, departmentId) values ('1', 'Joe', '70000', '1')
+insert into Employee (id, name, salary, departmentId) values ('2', 'Jim', '90000', '1')
+insert into Employee (id, name, salary, departmentId) values ('3', 'Henry', '80000', '2')
+insert into Employee (id, name, salary, departmentId) values ('4', 'Sam', '60000', '2')
+insert into Employee (id, name, salary, departmentId) values ('5', 'Max', '90000', '1')
+Truncate table Department
+insert into Department (id, name) values ('1', 'IT')
+insert into Department (id, name) values ('2', 'Sales')
+
+select Department.name AS 'Department', Employee.name AS 'Employee', Salary
+    from Employee join Department on Employee.DepartmentId = Department.Id
+        where (
+           Employee.DepartmentId, Salary) in (
+              select DepartmentId, max(Salary)
+                from Employee
+                    group by DepartmentId
+      )
+
+select d.name AS 'Department', e.name AS 'Employee', e.salary
+    from department d,employee e
+        where d.id = e.departmentId
+            and e.salary = (select max(e1.salary) from employee e1 where e1.departmentId = d.id)
