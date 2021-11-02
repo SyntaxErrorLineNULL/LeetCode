f5f7788b977270d89eb01eaaf0f19a657a43a9c1
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2021-11-02 23:31:19 +0300

    Employees Earning More Than Their Managers 620 problem

--- DataBase/solution.sql
+++ DataBase/solution.sql
@@ -45,3 +45,20 @@ select Email
     from Person
         group by Email
             having count(Email) > 1
+
+/**
+  Not Boring Movies
+ */
+Create table If Not Exists cinema (id int, movie varchar(255), description varchar(255), rating float(2, 1))
+Truncate table cinema
+insert into cinema (id, movie, description, rating) values ('1', 'War', 'great 3D', '8.9')
+insert into cinema (id, movie, description, rating) values ('2', 'Science', 'fiction', '8.5')
+insert into cinema (id, movie, description, rating) values ('3', 'irish', 'boring', '6.2')
+insert into cinema (id, movie, description, rating) values ('4', 'Ice song', 'Fantacy', '8.6')
+insert into cinema (id, movie, description, rating) values ('5', 'House card', 'Interesting', '9.1')
+
+select *
+    from cinema
+        where description <> 'boring' and id % 2
+        #where id % 2 and description != 'boring'
+            order by rating DESC
